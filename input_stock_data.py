5fd5d46bd4f106f677f8e0e9027dd2ebd018a0d1
HesilJames <hesil@163.com>
2018-08-09 16:32:47 +0800

    mod input_stock_data.py

--- input_stock_data.py
+++ input_stock_data.py
@@ -14,7 +14,6 @@ from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 
 
-
 class StockData:
 
     def __init__(self,code):
@@ -25,8 +24,30 @@ class StockData:
         self.daily=list(daily_data.find({'code' : code}).sort('date',pymongo.ASCENDING))
         self.basic=basic_data.find_one({'code':code})
 
+    def _read32(self):
+        res=[]
+        for d in self.daily:
+            try:
+                client = pymongo.MongoClient('localhost', 27017)#增加大盘数据用于对照
+                table_stock = client['stock']
+                daily_data = table_stock['daily']
+                b = daily_data.find_one({'code': '000001', 'date': d['date']})
+                if b == None:
+                    continue
+                else:
+                    res1d = [d['open'], d['close'], d['high'], d['low'], d['volume'], b['open'], b['close'], b['high'],
+                             b['low'], b['volume']]
+                    res.extend(res1d)
+            except:
+                print(d['date'])
+
+        return numpy.array(res).dtype(float)
+
+
+
+
 
 sd = StockData('000760')
-print(sd.basic['name'])
-print(sd.daily.__len__())
+r=sd._read32()
+print(r.shape)
 
